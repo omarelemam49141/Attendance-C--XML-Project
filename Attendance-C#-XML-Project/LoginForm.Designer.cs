fe31d4fade6ccf9fff149932b44e645766f27959
omarelemam49141 <114430558+omarelemam49141@users.noreply.github.com>
2024-02-16 11:48:46 +0200

    Adding the login form - Making the login form UI

---
+++ Attendance-C#-XML-Project/LoginForm.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Attendance_C__XML_Project
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginForm));
+            panel1 = new Panel();
+            label4 = new Label();
+            btnClose = new Button();
+            panel2 = new Panel();
+            pictureBox2 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            label3 = new Label();
+            txtUsername = new TextBox();
+            label2 = new Label();
+            lnkForgetPassword = new LinkLabel();
+            txtPassword = new TextBox();
+            label1 = new Label();
+            btnLogin = new Button();
+            pictureBox3 = new PictureBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(0, 7, 95);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(btnClose);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 37);
+            panel1.TabIndex = 0;
+            panel1.MouseDown += panel1_MouseDown;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(3, 7);
+            label4.Name = "label4";
+            label4.Size = new Size(123, 20);
+            label4.TabIndex = 8;
+            label4.Text = "Attendance Form";
+            // 
+            // btnClose
+            // 
+            btnClose.BackgroundImage = (Image)resources.GetObject("btnClose.BackgroundImage");
+            btnClose.BackgroundImageLayout = ImageLayout.Zoom;
+            btnClose.Cursor = Cursors.Hand;
+            btnClose.Dock = DockStyle.Right;
+            btnClose.FlatStyle = FlatStyle.Flat;
+            btnClose.Location = new Point(761, 0);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(39, 37);
+            btnClose.TabIndex = 0;
+            btnClose.UseVisualStyleBackColor = true;
+            btnClose.Click += btnClose_Click;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Transparent;
+            panel2.BackgroundImageLayout = ImageLayout.Center;
+            panel2.Controls.Add(pictureBox2);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(txtUsername);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(lnkForgetPassword);
+            panel2.Controls.Add(txtPassword);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(btnLogin);
+            panel2.Location = new Point(26, 106);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(318, 293);
+            panel2.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(17, 164);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(31, 25);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 7;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(17, 106);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(31, 25);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(143, 11);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Login";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(54, 108);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(248, 23);
+            txtUsername.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(54, 90);
+            label2.Name = "label2";
+            label2.Size = new Size(60, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Username";
+            // 
+            // lnkForgetPassword
+            // 
+            lnkForgetPassword.AutoSize = true;
+            lnkForgetPassword.DisabledLinkColor = Color.White;
+            lnkForgetPassword.ForeColor = Color.White;
+            lnkForgetPassword.LinkColor = Color.White;
+            lnkForgetPassword.Location = new Point(119, 254);
+            lnkForgetPassword.Name = "lnkForgetPassword";
+            lnkForgetPassword.Size = new Size(94, 15);
+            lnkForgetPassword.TabIndex = 3;
+            lnkForgetPassword.TabStop = true;
+            lnkForgetPassword.Text = "Forget password";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(54, 166);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(248, 23);
+            txtPassword.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(54, 148);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Password";
+            // 
+            // btnLogin
+            // 
+            btnLogin.FlatAppearance.BorderColor = Color.White;
+            btnLogin.FlatStyle = FlatStyle.Flat;
+            btnLogin.ForeColor = Color.White;
+            btnLogin.Location = new Point(101, 205);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(130, 38);
+            btnLogin.TabIndex = 0;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Dock = DockStyle.Fill;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(0, 0);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(800, 513);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(800, 513);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox3);
+            DoubleBuffered = true;
+            Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "LoginForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "LoginForm";
+            Load += LoginForm_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button btnClose;
+        private Panel panel2;
+        private Label label3;
+        private TextBox txtUsername;
+        private Label label2;
+        private LinkLabel lnkForgetPassword;
+        private TextBox txtPassword;
+        private Label label1;
+        private Button btnLogin;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox3;
+        private Label label4;
+    }
+}
---
+++ Attendance-C#-XML-Project/LoginForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Attendance_C__XML_Project
+{
+    public partial class LoginForm : Form
+    {
+        // Import the necessary Windows API methods
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HT_CAPTION = 0x2;
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            GraphicsPath path = new GraphicsPath();
+            Rectangle bounds = new Rectangle(0, 0, this.Width, this.Height);
+            int radius = 10; // Change this value to adjust the roundness of the corners
+
+            int diameter = radius * 2;
+            Size size = new Size(diameter, diameter);
+            Rectangle arc = new Rectangle(bounds.Location, size);
+
+            // Add arcs to the path to create rounded corners
+            path.AddArc(arc, 180, 90);
+            arc.X = bounds.Right - diameter;
+            path.AddArc(arc, 270, 90);
+            arc.Y = bounds.Bottom - diameter;
+            path.AddArc(arc, 0, 90);
+            arc.X = bounds.Left;
+            path.AddArc(arc, 90, 90);
+
+            // Close the path
+            path.CloseFigure();
+
+            // Draw the form using the path
+            this.Region = new Region(path);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            // Check if the left mouse button is pressed
+            if (e.Button == MouseButtons.Left)
+            {
+                // Release capture to enable normal mouse operation like clicking on buttons
+                ReleaseCapture();
+
+                // Send the message to move the form
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+            }
+        }
+    }
+}
