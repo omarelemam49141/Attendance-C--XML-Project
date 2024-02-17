eb61cff5641cedc4d5dd0be25597145192e70979
omarelemam49141 <114430558+omarelemam49141@users.noreply.github.com>
2024-02-17 17:17:49 +0200

    adding student form raw files

---
+++ Attendance-C#-XML-Project/StudentForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Attendance_C__XML_Project
+{
+    public partial class StudentForm : Form
+    {
+        public StudentForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
